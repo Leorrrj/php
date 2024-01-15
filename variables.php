062cbaa93044a13a5db883887f37f546d6734eb4
Leorrrj <leonidasml.2@gmail.com>
2024-01-14 22:07:31 -0300

    php

---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+    $nombre= "Leonardo "; //string
+    $apellido= "Muños López"; //string
+    $casado= true; //boolen
+    $edad= 32; //int
+    echo $nombre;
+    print_r($apellido);
+    ?>
